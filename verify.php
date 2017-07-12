04ebe76c0ac8d98524dc2457e804932ce8ae89b6
paulineordenes <jan_ordenes@dlsu.edu.ph>
2017-07-12 14:49:13 +0800

    Delete verify.php

--- verify.php
+++
@@ -1,6 +0,0 @@
-<?php
-$username=$_POST['username'];
-$password=$_POST['password'];
-//insert password and username checking here
-$sql="SELECT COUNT(*) FROM accounts WHERE username='".$username." AND password='".$password."';";
-?>
